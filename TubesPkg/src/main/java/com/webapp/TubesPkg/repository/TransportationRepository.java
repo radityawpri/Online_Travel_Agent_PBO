942c114e2a5f48139fa66e0013cb8226bec1ec9f
raps1304 <rafdirp@gmail.com>
2025-01-06 00:14:56 +0700

    Final Admin Done.

--- TubesPkg/src/main/java/com/webapp/TubesPkg/repository/TransportationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webapp.TubesPkg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.webapp.TubesPkg.models.Transportation;
-
-public interface TransportationRepository extends JpaRepository<Transportation, Integer>{  
-} 
